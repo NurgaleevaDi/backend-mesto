f4af005b7b8dfd25c7fa9752ae4c337c69ee2164
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-14 22:20:43 +0300

    PR 15 logger, cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
 const NotFoundError = require('./errors/not-found-error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const PORT = 3000;
@@ -16,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(requestLogger); // подключаем логгер запросов до всех обработчиков роутов
 
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
@@ -43,6 +46,8 @@ app.post(
   }),
   createUser,
 );
+
+app.use(errorLogger); // подключаем логгер ошибок после обработчиков роутов и до обработчиков ошибок
 
 app.use('/*', (req, res, next) => next(new NotFoundError('Запрашиваемая страница не существует')));
 // res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемая страница не существует' });
