0e608706bb96b0e872309271792135f3fd2e717f
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-15 14:49:46 +0300

    PR 15 cors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const { errors, celebrate, Joi } = require('celebrate');
 const NotFoundError = require('./errors/not-found-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const { cors } = require('./middlewares/cors');
+const { cors } = require('./middlewares/cors');
 
 // const allowedCors = [
 //   'localhost:3000',
@@ -22,8 +22,8 @@ const {
 } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 
-// app.use(cors);
-app.use(cors());
+app.use(cors);
+// app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
