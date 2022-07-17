4509b59bf08bab611f70aaf01f2cf4e27b26c120
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-17 20:41:32 +0300

    PR 15 env-file

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -6,9 +6,10 @@ const SECRET_KEY = 'very-secret';
 // const generateToken = (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: '7d' });
 const generateToken = (payload) => jwt.sign(
   payload,
-  NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+  NODE_ENV === 'production' ? JWT_SECRET : SECRET_KEY,
   { expiresIn: '7d' },
 );
-const checkToken = (token) => jwt.verify(token, SECRET_KEY);
+// const checkToken = (token) => jwt.verify(token, SECRET_KEY);
+const checkToken = (token) => jwt.verify(token, NODE_ENV ? JWT_SECRET : SECRET_KEY);
 
 module.exports = { generateToken, checkToken };
