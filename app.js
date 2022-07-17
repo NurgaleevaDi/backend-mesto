10a00b166c05c07675a7d9d5c2a9422869299d39
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-17 19:11:34 +0300

    PR 15 crash test

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(requestLogger); // подключаем логгер запросов д
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
 
+// краш-тест сервера
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post(
   '/signin',
   celebrate({
