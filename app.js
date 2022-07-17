f7bf27de1526ec09f2cae3107c4f85a79707b3bf
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-17 22:15:08 +0300

    + env

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// require('dotenv').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
@@ -7,12 +7,11 @@ const NotFoundError = require('./errors/not-found-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { cors } = require('./middlewares/cors');
 
-console.log('NODE');
 // console.log('env ', process.env.NODE_ENV);
 
 const app = express();
-// const { PORT = 3000 } = process.env;
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
+// const PORT = 3000;
 const {
   createUser,
   login,
