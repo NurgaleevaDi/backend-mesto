f5309a2d79fd690939ad7ce3d0f852218c457eec
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-18 11:12:33 +0300

    Fix controllers-user

--- controllers/users.js
+++ controllers/users.js
@@ -112,18 +112,17 @@ module.exports.login = (req, res, next) => {
         throw new Unauthorized('Не передан email или password');
       }
       return generateToken({ _id: user._id });
-      // return generateToken({ email: user.email });
     })
     .then((token) => {
       res.send({ token });
     })
-    // .catch((err) => {
-    //   if (err.statusCode === 403) {
-    //     next(new ForbiddenError('Необходимо авторизоваться'));
-    //   }
-    //   next(err);
-    // });
-    .catch(next);
+    .catch((err) => {
+      if (err.statusCode === 403) {
+        next(new ForbiddenError('Необходимо авторизоваться'));
+      }
+      next(err);
+    });
+// .catch(next);
 };
 
 module.exports.updateUser = (req, res, next) => {
